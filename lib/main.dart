507b189c5644794e19761d322a216f56973dc4cc
Melina Kubota <melina.kubota@gmail.com>
2021-08-16 12:51:46 -0300

    Arrumando firabese para integração

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(); 
   FirebaseFirestore db = FirebaseFirestore.instance;
-  db.collection("usuarios").doc("159").set({
+  db.collection("usuarios").doc("1291").set({
     "nome": "Melina", 
     "idade": "28",
   });
